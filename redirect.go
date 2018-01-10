26dc808ff5ce531a8098e3daff245867ff354cd9
Jordan Day <jordanday444@gmail.com>
2018-01-10 09:19:11 -0600

    Don't return callback and remove duplicate code

--- redirect.go
+++ redirect.go
@@ -2,18 +2,15 @@ package router
 
 import (
 	"github.com/gopherjs/gopherjs/js"
-	"github.com/gopherjs/vecty"
 )
 
 // Redirect ...
-func Redirect(route string) EventCallback {
-	return func(e *vecty.Event) {
-		js.Global.Get("history").Call(
-			"pushState",
-			map[string]string{"redirectRoute": route},
-			route,
-			route,
-		)
-		refreshRoutes()
-	}
+func Redirect(route string) {
+	js.Global.Get("history").Call(
+		"pushState",
+		map[string]string{"redirectRoute": route},
+		route,
+		route,
+	)
+	refreshRoutes()
 }
